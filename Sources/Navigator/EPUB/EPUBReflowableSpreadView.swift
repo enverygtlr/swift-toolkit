637727faf17ed027da9b4a160d7c7da082dff781
Enver Yigitler <enverygtlr@hotmail.com>
2023-07-18 10:00:18 +0300

    scrollView insets changed.

--- Sources/Navigator/EPUB/EPUBReflowableSpreadView.swift
+++ Sources/Navigator/EPUB/EPUBReflowableSpreadView.swift
@@ -136,10 +136,14 @@ final class EPUBReflowableSpreadView: EPUBSpreadView {
             // Increases the insets by the notch area (eg. iPhone X) to make sure that the content is not overlapped by the screen notch.
             insets.top += notchAreaInsets.top
             insets.bottom += notchAreaInsets.bottom
-
-            topConstraint.constant = insets.top
-            bottomConstraint.constant = -insets.bottom
-            scrollView.contentInset = .zero
+//
+//            topConstraint.constant = insets.top
+//            bottomConstraint.constant = -insets.bottom
+//            scrollView.contentInset = .zero
+            topConstraint.constant = 0
+            bottomConstraint.constant = 0
+            scrollView.contentInset = UIEdgeInsets(top: insets.top, left: 0, bottom: insets.bottom, right: 0)
+            
         }
     }
 
