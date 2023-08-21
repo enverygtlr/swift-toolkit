d292179b94e4bdd99390054bb3afcf26102c254c
Enver Yigitler <enverygtlr@hotmail.com>
2023-08-21 16:13:05 +0300

    automaticallyWaitsToMinimizeStalling set to false

--- Sources/Navigator/Audiobook/AudioNavigator.swift
+++ Sources/Navigator/Audiobook/AudioNavigator.swift
@@ -97,7 +97,7 @@ open class AudioNavigator: MediaNavigator, AudioSessionUser, Loggable {
     private lazy var player: AVPlayer = {
         let player = AVPlayer()
         player.allowsExternalPlayback = false
-        player.automaticallyWaitsToMinimizeStalling = true
+        player.automaticallyWaitsToMinimizeStalling = false
 
         player.addPeriodicTimeObserver(forInterval: CMTime(seconds: 0.5, preferredTimescale: 1000), queue: .main) { [weak self] time in
             if let self = self {
