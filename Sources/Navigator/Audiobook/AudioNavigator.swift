273283d36e44373e7c1eacc6c53ffabb64eb21c2
Enver Yigitler <enverygtlr@hotmail.com>
2023-08-27 15:49:06 +0300

    adsfasdf

--- Sources/Navigator/Audiobook/AudioNavigator.swift
+++ Sources/Navigator/Audiobook/AudioNavigator.swift
@@ -258,7 +258,7 @@ open class AudioNavigator: MediaNavigator, AudioSessionUser, Loggable {
             if player.currentItem == nil || resourceIndex != newResourceIndex {
                 log(.info, "Starts playing \(link.href)")
                 let originalURL = link.url(relativeTo: publication.baseURL) ?? URL(fileURLWithPath: link.href)
-                let asset = AVAsset(url: originalURL)
+                let asset = AVURLAsset(url: originalURL, options: [AVURLAssetPreferPreciseDurationAndTimingKey: true])
                 
                 player.replaceCurrentItem(with: AVPlayerItem(asset: asset))
                 resourceIndex = newResourceIndex
